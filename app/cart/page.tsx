506bea668b2ed57e010fb0f85b43fdb964575dd2
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-06-16 14:57:09 +0100

    Create page.tsx

---
+++ app/cart/page.tsx
@@ -0,0 +1,9 @@
+export default function CartPage() {
+  return (
+    <main className="mx-auto max-w-7xl p-8">
+      <h1 className="mb-6 text-3xl font-bold">Shopping Cart</h1>
+
+      <p>Your cart is empty.</p>
+    </main>
+  );
+}
