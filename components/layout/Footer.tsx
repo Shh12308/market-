343775255f828ec8c81c37ce0ffce4db784e95e9
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-06-16 14:42:24 +0100

    Create Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20 border-t p-6 text-center text-gray-500">
+      © 2026 Marketplace. All rights reserved.
+    </footer>
+  );
+}
