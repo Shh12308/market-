ab603409da7759688661a81ed98c031703d3fbdf
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-06-18 22:41:53 +0100

    Create page.tsx

---
+++ app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+export default function Success() {
+  return (
+    <div className="text-center p-20">
+      <h1 className="text-5xl font-bold text-green-600">
+        Payment Successful
+      </h1>
+    </div>
+  );
+}
