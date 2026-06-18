4843829bed0e6760bcce790f532a190b7321b711
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-06-18 22:42:16 +0100

    Create page.tsx

---
+++ app/payment/failure/page.tsx
@@ -0,0 +1,9 @@
+export default function Failure() {
+  return (
+    <div className="text-center p-20">
+      <h1 className="text-5xl font-bold text-red-600">
+        Payment Failed
+      </h1>
+    </div>
+  );
+}
